2c0a646a5e70e24a3dff78d7adc1d7466793d7b5
Sergey Scherbina <sergey.scherbina@gmail.com>
2013-01-10 01:45:53 +0200

    fields types

--- org.tmatesoft.jlibc/src/main/org/tmatesoft/jlibc/Struct.java
+++ org.tmatesoft.jlibc/src/main/org/tmatesoft/jlibc/Struct.java
@@ -82,8 +82,92 @@ public abstract class Struct {
 			return struct().pointer().getByte(offset());
 		}
 
-		public void set(byte value) {
+		public void set(final byte value) {
 			struct().pointer().setByte(offset(), value);
+		}
+	}
+
+	public static class SignedShort extends Field {
+		public SignedShort(final Struct struct) {
+			super(struct, 2);
+		}
+
+		public short get() {
+			return struct().pointer().getShort(offset());
+		}
+
+		public void set(final short value) {
+			struct().pointer().setShort(offset(), value);
+		}
+	}
+
+	public static class SignedInt extends Field {
+		public SignedInt(final Struct struct) {
+			super(struct, 4);
+		}
+
+		public int get() {
+			return struct().pointer().getInt(offset());
+		}
+
+		public void set(final int value) {
+			struct().pointer().setInt(offset(), value);
+		}
+	}
+
+	public static class SignedLong extends Field {
+		public SignedLong(final Struct struct) {
+			super(struct, 4);
+		}
+
+		public long get() {
+			return struct().pointer().getLong(offset());
+		}
+
+		public void set(final long value) {
+			struct().pointer().setLong(offset(), value);
+		}
+	}
+
+	public static class UnsignedByte extends Field {
+		public UnsignedByte(final Struct struct) {
+			super(struct, 1);
+		}
+
+		public short get() {
+			return struct().pointer().getUnsignedByte(offset());
+		}
+
+		public void set(final short value) {
+			struct().pointer().setUnsignedByte(offset(), value);
+		}
+	}
+
+	public static class UnsignedShort extends Field {
+		public UnsignedShort(final Struct struct) {
+			super(struct, 2);
+		}
+
+		public int get() {
+			return struct().pointer().getUnsignedShort(offset());
+		}
+
+		public void set(final int value) {
+			struct().pointer().setUnsignedShort(offset(), value);
+		}
+	}
+
+	public static class UnsignedInt extends Field {
+		public UnsignedInt(final Struct struct) {
+			super(struct, 4);
+		}
+
+		public long get() {
+			return struct().pointer().getUnsignedInt(offset());
+		}
+
+		public void set(final long value) {
+			struct().pointer().setUnsignedInt(offset(), value);
 		}
 	}
 
---
+++ org.tmatesoft.jlibc/src/test/org/tmatesoft/jlibc/test/StructTest2.java
@@ -0,0 +1,107 @@
+package org.tmatesoft.jlibc.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.tmatesoft.jlibc.Memory;
+import org.tmatesoft.jlibc.Struct;
+import org.tmatesoft.jlibc.Memory.Pointer;
+
+public class StructTest2 {
+
+	private final Memory memory = new Memory.ByteArray(1024);
+	private final Pointer pointer = new Pointer(memory, 0);
+	private TestStruct struct = new TestStruct(pointer);
+
+	@Before
+	public void setUp() {
+		struct.f1().set((byte) 1);
+		struct.s1().f1().set((byte) 2);
+		struct.s1().f2().set((byte) 3);
+		struct.f2().set((byte) 4);
+		struct.s2().f1().set((byte) 5);
+		struct.s2().f2().set((byte) 6);
+	}
+
+	@Test
+	public void testOffset() {
+		assertEquals(0, struct.f1().offset());
+		assertEquals(1, struct.s1().f1().offset());
+		assertEquals(2, struct.s2().f2().offset());
+		assertEquals(3, struct.f2().offset());
+		assertEquals(4, struct.s2().f1().offset());
+		assertEquals(5, struct.s2().f2().offset());
+	}
+
+	@Test
+	public void testSet() {
+		assertEquals(1, memory.getByte(0));
+		assertEquals(2, memory.getByte(1));
+		assertEquals(3, memory.getByte(2));
+		assertEquals(4, memory.getByte(3));
+		assertEquals(5, memory.getByte(4));
+		assertEquals(6, memory.getByte(5));
+	}
+
+	@Test
+	public void testSetGet() {
+		assertEquals(1, struct.f1().get());
+		assertEquals(2, struct.s1().f1().get());
+		assertEquals(3, struct.s1().f2().get());
+		assertEquals(4, struct.f2().get());
+		assertEquals(5, struct.s2().f1().get());
+		assertEquals(6, struct.s2().f2().get());
+	}
+
+	static class TestStruct extends Struct {
+
+		public TestStruct(final Pointer pointer) {
+			super(pointer);
+		}
+
+		public static class TestSubStruct extends Struct {
+
+			private UnsignedByte f1 = new UnsignedByte(this);
+			private UnsignedInt f2 = new UnsignedInt(this);
+
+			public TestSubStruct(final TestStruct parent) {
+				super(parent);
+			}
+
+			public UnsignedByte f1() {
+				return f1;
+			}
+
+			public UnsignedInt f2() {
+				return f2;
+			}
+		}
+
+		private UnsignedByte f1 = new UnsignedByte(this);
+
+		private TestSubStruct s1 = new TestSubStruct(this);
+
+		private UnsignedShort f2 = new UnsignedShort(this);
+
+		private TestSubStruct s2 = new TestSubStruct(this);
+
+		public UnsignedByte f1() {
+			return f1;
+		}
+
+		public TestSubStruct s1() {
+			return s1;
+		}
+
+		public UnsignedShort f2() {
+			return f2;
+		}
+
+		public TestSubStruct s2() {
+			return s2;
+		}
+
+	}
+
+}
